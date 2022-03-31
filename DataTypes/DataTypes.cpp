97a827a3e7ee4ce40a8745962afff1a1b4178d21
AReeme <areeme@student.neumont.edu>
2022-03-31 17:44:25 -0600

    Primitive Data Types Assignment

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -6,43 +6,51 @@
 
 using namespace std;
 
-namespace reeme
-{
-    int i = 10;
-}
-
 int main()
 {
-    int i = 0;
-    float f = 23;
-    bool b = 12;
     string name;
-
-    const float TAX = 0.085;
     char initial;
-
-
-    int ages[4]; 
-    ages[0] = 24;
-    ages[1] = 10;
-    ages[2] = 32;
-
+    int age;
+    bool isAdult;
+    string zipcode;
+    float wage;
+    int daysWorked;
+    int hoursWorkedPerDay[10];
+    const float TAX = 0.1f;
+    int totalHours = 0;
     
+
+    cout << "enter first name: ";
     cin >> name;
-    cout << name;
-    
-    if (i) cout << "true\n";
-    cout << reeme::i << endl;
-    cout << "Hello Data!\n";
+
+    cout << "enter last initial: ";
+    cin >> initial;
+
+    cout << "enter age: ";
+    cin >> age;
+    if (age > 18) isAdult = true;
+    else isAdult = false;
+
+    cout << "enter zipcode: ";
+    cin >> zipcode;
+
+    cout << "enter wage: ";
+    cin >> wage;
+
+    cout << "enter days worked: ";
+    cin >> daysWorked;
+    for (int i = 1; i < daysWorked + 1; i++)
+    {
+        cout << "enter hours worked for day " << i << ": ";
+        cin >> hoursWorkedPerDay[i];
+        totalHours = totalHours + hoursWorkedPerDay[i];
+    }
+
+    int gross = totalHours * wage;
+    int grossTax = gross * TAX;
+    int netIncome = gross - grossTax;
+
+    cout << name << " worked " << totalHours << " hours at $" << wage << " an hour.\n";
+    cout << "gross income: $" << gross;
+    cout << "\nnet income: $" << netIncome;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
